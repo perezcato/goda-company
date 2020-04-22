ee67377d07f1031efd529e03bb6ce432b000a48d
Perez Cato <perezcatoc@gmail.com>
2020-04-22 20:26:25 +0000

    refactored the header

--- components/nav.js
+++ components/nav.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 
 export default () => (
-    <header className="grid grid-cols-4 w-full px-32 py-6">
+    <header className="grid grid-cols-4 w-full px-32 py-4 border-b border-gray-500">
         <div className="icon">
             <img src="/images/logo.png" className="w-32" alt="logo"/>
         </div>
@@ -26,10 +26,10 @@ export default () => (
 
         </div>
         <div className="auth-buttons m-auto">
-            <button className="py-3 px-4 border-2 mr-5">
+            <button className="py-3 px-4 border border-gray-700 mr-5 ">
                 Sign In
             </button>
-            <button className="py-3 px-4 border-2">
+            <button className="py-3 px-4 border-gray-700 border">
                 Register
             </button>
         </div>
